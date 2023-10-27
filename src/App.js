cd7595f043ad2a57d2e6f497f6e60764edc338fe
Nele31 <131680345+Nele31@users.noreply.github.com>
2023-10-26 16:41:20 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react';
+import Blog from './public/blog';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>New title</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Blog />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
