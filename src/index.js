5f215f07b40a10269b88b7780338fc07360d03c4
DESKTOP-5OJ7N1J <abauman3@my.hpu.edu>
2023-11-04 23:53:00 -1000

    Some login features

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import Layout from './Layout'; // Import the RedditLayout component
+import Layout from './Layout';
 import reportWebVitals from './a/reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
-
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
+import NotFound from './components/NotFound';
+import CreateAccount from './components/CreateAccount';
 import LoginForm from './components/LoginForm';
-import NotFound from "./components/NotFound"
 
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleSignIn = () => {
+    // Simulated sign-in logic
+    // Set isAuthenticated to true upon successful sign-in
+    setIsAuthenticated(true);
+  };
+
+  const handleSignOut = () => {
+    // Implement your sign-out logic here
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="/" element={<Layout isAuthenticated={isAuthenticated} onSignOut={handleSignOut} />} />
+        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/Page2" element={<Page2 />} />
+        <Route path="/Page3" element={<Page3 />} />
+        <Route path="/login" element={<LoginForm onSignIn={handleSignIn} />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  <BrowserRouter>
-    <Routes>
-          <Route exact path="/" element={<Layout />} />
-          <Route path="/Page1" element={<Page1 />} />
-          <Route path="/Page2" element={<Page2 />} />
-          <Route path="/Page3" element={<Page3 />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-  </BrowserRouter>
-
-
-);
-
+root.render(<App />);
 reportWebVitals();
