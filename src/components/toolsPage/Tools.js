fd650ae1dd5fefe8d87dadde1c74c31ebceb7b3f
Kristian Birkeli <kristian.birkeli@gmail.com>
2023-11-07 12:46:05 -1000

    create basic structure of the tools page

---
+++ src/components/toolsPage/Tools.js
@@ -0,0 +1,44 @@
+import React from "react"
+const Tools = () => {
+  return (
+    <div className='toolsContainer'>
+      <section className='toolsList'>
+        {tools.map((tool) => {
+          return <Tool {...tools} key={tool.id} />
+        })}
+      </section>
+    </div>
+  )
+}
+
+const Tool = ({ toolName, link, image }) => {
+  return (
+    <article className='tool'>
+      <img src={image} alt={toolName} />
+      <h2>{toolName}</h2>
+      <h4>{link}</h4>
+    </article>
+  )
+}
+
+const tools = [
+  {
+    toolName: "tool 1",
+    link: "link",
+    image: "image",
+    id: 1,
+  },
+  {
+    toolName: "tool 2",
+    link: "link",
+    image: "image",
+    id: 2,
+  },
+  {
+    toolName: "tool 3",
+    link: "link",
+    image: "image",
+    id: 3,
+  },
+]
+export default Tools
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import Layout from './Layout'; // Import the RedditLayout component
-import reportWebVitals from './a/reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./style.css"
+import Layout from "./Layout" // Import the RedditLayout component
+import reportWebVitals from "./a/reportWebVitals"
+import { BrowserRouter } from "react-router-dom"
 
-import { Route, Routes } from 'react-router-dom';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
-import LoginForm from './components/LoginForm';
+import { Route, Routes } from "react-router-dom"
+import Page1 from "./components/Page1"
+import Page2 from "./components/Page2"
+import Page3 from "./components/Page3"
+import LoginForm from "./components/LoginForm"
 import NotFound from "./components/NotFound"
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-
   <BrowserRouter>
     <Routes>
-          <Route exact path="/" element={<Layout />} />
-          <Route path="/Page1" element={<Page1 />} />
-          <Route path="/Page2" element={<Page2 />} />
-          <Route path="/Page3" element={<Page3 />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <Route exact path='/' element={<Layout />} />
+      <Route path='/Page1' element={<Page1 />} />
+      <Route path='/Page2' element={<Page2 />} />
+      <Route path='/Page3' element={<Page3 />} />
+      <Route path='/login' element={<LoginForm />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   </BrowserRouter>
+)
 
-
-);
-
-reportWebVitals();
+reportWebVitals()
